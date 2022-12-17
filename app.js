1024a2d178fae0d3804e5d9716512d8458ba6c74
Sachin Wanniarachchi <sachinwanniarachchi@gamil.com>
2022-12-17 19:05:30 +0530

    Add routes and middlewares

--- app.js
+++ app.js
@@ -3,20 +3,24 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-//Config .env
-dotenv.config();
-
 // Crete a Express Framework
 const app = express();
 
-// Declare a PORT
-const PORT = process.env.APP_RUNNING_PORT || 3000;
-
 //Add Routes
+const resturentRouter = require("./src/routes/resturent");
+const userRouter = require("./src/routes/user");
 
 // Add Middlewares
 app.use(cors());
 app.use(express.json());
+app.use("/api/resturents", resturentRouter);
+app.use("/api/user", userRouter);
+
+//Config .env
+dotenv.config();
+
+// Declare a PORT
+const PORT = process.env.APP_RUNNING_PORT || 3000;
 
 // Check runing port
 app.listen(PORT, () => {
