fcc39226e73a8152dbb51c01632c17acd0be2dc0
Sachin Wanniarachchi <sachinwanniarachchi@gamil.com>
2023-01-10 06:30:36 +0530

    Create a food model

---
+++ src/models/food.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const foodSchema = new mongoose.Schema({
+  itemId: { type: String, required: true, unique: true },
+  name: { type: String, required: true, minlength: 5 },
+  mealType: { type: String, required: true },
+  category: { type: String, required: true },
+  price: { type: Number, required: true },
+  rating: { type: Number, required: true, min: 0, max: 5 },
+  imageUrl: { type: String, required: true },
+  description: { type: String, required: false, maxlength: 250 },
+  isActive: { type: Boolean, required: true, default: true },
+  createdDateTime: { type: Date, default: Date.now() },
+});
+
+// Create an instance of model Outlet
+const Food = mongoose.model("food", foodSchema);
+
+module.exports = Food;
