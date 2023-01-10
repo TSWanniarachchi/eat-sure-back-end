60fdc038d4d8da4de59ddbb15b6901a375f1cc0d
Sachin Wanniarachchi <sachinwanniarachchi@gamil.com>
2023-01-10 22:34:32 +0530

    Updated food route

--- src/routes/food.js
+++ src/routes/food.js
@@ -2,6 +2,27 @@ const express = require("express");
 const foodRouter = express.Router();
 const foodModel = require("../models/food");
 
+// Insert food detail
+foodRouter.post("/", async (req, res) => {
+  try {
+    const food = new foodModel({
+      itemId: req.body.itemId,
+      name: req.body.name,
+      mealType: req.body.mealType,
+      category: req.body.category,
+      price: req.body.price,
+      rating: req.body.rating,
+      imageUrl: req.body.imageUrl,
+      description: req.body.description,
+    });
+
+    const newfood = await food.save();
+    res.status(200).send(newfood);
+  } catch (err) {
+    return res.status(500).send(`Error: ${err.message}`);
+  }
+});
+
 // Get all food details
 foodRouter.get("/", async (req, res) => {
   try {
@@ -37,7 +58,7 @@ foodRouter.get("/:foodId", async (req, res) => {
 });
 
 // Get food details by meal type
-foodRouter.get("/:mealType", async (req, res) => {
+foodRouter.get("/:mealType/mealType", async (req, res) => {
   try {
     let foods = await foodModel.find({
       mealType: req.params.mealType,
@@ -59,7 +80,7 @@ foodRouter.get("/:mealType", async (req, res) => {
 });
 
 // Get food details by category
-foodRouter.get("/:category", async (req, res) => {
+foodRouter.get("/:category/category", async (req, res) => {
   try {
     let foods = await foodModel.find({
       category: req.params.category,
