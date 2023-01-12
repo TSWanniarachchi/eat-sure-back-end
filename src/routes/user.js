b1e0e6f43eb114166a633a487ede1ad92a723243
Sachin Wanniarachchi <sachinwanniarachchi@gamil.com>
2023-01-12 18:16:44 +0530

    Add update and delete methads

--- src/routes/user.js
+++ src/routes/user.js
@@ -22,7 +22,6 @@ userRouter.post("/", async (req, res) => {
     const newUser = await user.save();
     res.status(200).send(newUser);
   } catch (err) {
-    l;
     return res.status(500).send(`Error: ${err.message}`);
   }
 });
@@ -59,6 +58,124 @@ userRouter.get("/:username/:password", async (req, res) => {
     res.status(200).send(user);
   } catch (err) {
     return res.status(500).send(`Error: ${err.message}`);
+  }
+});
+
+//update user details by username
+userRouter.put("/:username", async (req, res) => {
+  try {
+    let user = await userModel.findOne({
+      username: req.params.username,
+    });
+
+    if (!user) {
+      let errorObj = {
+        message:
+          "The given username and password does not match any user on our system",
+        statusCode: "NOT FOUND",
+      };
+      return res.status(404).send(errorObj);
+    } else {
+      let isChange = false;
+
+      if (user.fullName != req.body.fullName) {
+        user.fullName = req.body.fullName;
+        isChange = true;
+      }
+      if (user.dateOfBirth != req.body.dateOfBirth) {
+        user.dateOfBirth = req.body.dateOfBirth;
+        isChange = true;
+      }
+      if (user.gender != req.body.gender) {
+        user.gender = req.body.gender;
+        isChange = true;
+      }
+      if (user.location.latitude != req.body.location.latitude) {
+        user.location.latitude = req.body.location.latitude;
+        isChange = true;
+      }
+      if (user.location.longitude != req.body.location.longitude) {
+        user.location.longitude = req.body.location.longitude;
+        isChange = true;
+      }
+      if (user.contactNo != req.body.contactNo) {
+        user.contactNo = req.body.contactNo;
+        isChange = true;
+      }
+      if (user.email != req.body.email) {
+        user.email = req.body.email;
+        isChange = true;
+      }
+
+      if (isChange) {
+        let updateUser = await user.save();
+        // res.status(200).send(updateUser);
+        res.status(200).send("Successfully Updated!");
+      }
+    }
+  } catch (ex) {
+    return res.status(500).send(`Error: ${ex.message}`);
+  }
+});
+
+//update user password by username and oldPassword
+userRouter.put("/:username/:oldPassword", async (req, res) => {
+  try {
+    let user = await userModel.findOne({
+      username: req.params.username,
+      password: req.params.oldPassword,
+    });
+
+    if (!user) {
+      let errorObj = {
+        message:
+          "The given username and password does not match any user on our system",
+        statusCode: "NOT FOUND",
+      };
+      return res.status(404).send(errorObj);
+    } else {
+      let isChange = false;
+
+      if (user.password != req.body.newPassword) {
+        user.password = req.body.newPassword;
+        isChange = true;
+      } else {
+        res.status(200).send("New Password and Old Password are Same.");
+      }
+
+      if (isChange) {
+        let updateUser = await user.save();
+        // res.status(200).send(updateUser);
+        res.status(500).send("Password Successfully Updated!");
+      }
+    }
+  } catch (ex) {
+    return res.status(500).send(`Error: ${ex.message}`);
+  }
+});
+
+// Delete user details by username
+userRouter.delete("/:username", async (req, res) => {
+  try {
+    let user = await userModel.findOne({
+      username: req.params.username,
+    });
+
+    if (!user) {
+      let errorObj = {
+        message: "The given username does not match any user on our system",
+        statusCode: "NOT FOUND",
+      };
+      return res.status(404).send(errorObj);
+    }
+
+    const deleteuser = await userModel.deleteOne({
+      username: req.params.username,
+    });
+    //res.status(200).json(deleteFoodOutlet);
+    res.status(200).send("Successfully Deleted!");
+  } catch (ex) {
+    return res.status(500).send(`Error: ${ex.message}`);
   }
 });
 
