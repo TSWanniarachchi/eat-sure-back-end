df53da0d40d936059fa316c8f1279095d9382cdf
Sachin Wanniarachchi <sachinwanniarachchi@gamil.com>
2023-01-13 17:22:41 +0530

    Added ingredients & nutrition fields to food modle

--- src/models/food.js
+++ src/models/food.js
@@ -1,11 +1,41 @@
 const mongoose = require("mongoose");
 
+// Location Field
+const locationField = mongoose.Schema(
+  {
+    latitude: { type: Number, required: true },
+    longitude: { type: Number, required: true },
+  },
+  { _id: false }
+);
+
+// Nutrition Facts Field
+const NutritionFactsField = mongoose.Schema(
+  {
+    servingSize: { type: Number, required: true, minlength: 2 },
+    calories: { type: Number, required: true, minlength: 4 },
+    totalFat: { type: Number, required: true, minlength: 4 },
+    saturatedFat: { type: Number, required: true, minlength: 4 },
+    cholesterol: { type: Number, required: true, minlength: 4 },
+    sodium: { type: Number, required: true, minlength: 4 },
+    carbohydrate: { type: Number, required: true, minlength: 4 },
+    sugars: { type: Number, required: true, minlength: 4 },
+    protein: { type: Number, required: true, minlength: 4 },
+    vitamin: { type: Number, required: true, minlength: 4 },
+    calcium: { type: String, required: true, minlength: 10 },
+    Iron: { type: Number, required: true, minlength: 4 },
+  },
+  { _id: false }
+);
+
 const foodSchema = new mongoose.Schema({
   foodId: { type: String, required: true, unique: true },
   name: { type: String, required: true, minlength: 5 },
+  cuisinesType: { type: String, required: true },
   mealType: { type: String, required: true },
   category: { type: String, required: true },
-  price: { type: Number, required: true },
+  ingredients: { type: Array, required: true, default: [] },
+  nutritionFacts: NutritionFactsField,
   rating: { type: Number, required: true, min: 0, max: 5 },
   imageUrl: { type: String, required: true },
   description: { type: String, required: false, maxlength: 250 },
